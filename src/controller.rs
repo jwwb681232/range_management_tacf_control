12b220e430a4f13d8a7e12ed037258dfdaf46b39
wangxiao <wangxiao@guadoutech.com>
2021-12-23 13:25:09 +0800

    serde

--- src/controller.rs
+++ src/controller.rs
@@ -1,5 +1,5 @@
 use std::fmt::{Display, Formatter};
-use serde_json::{Serialize,Deserialize};
+use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct NopAck {
@@ -8,16 +8,21 @@ pub struct NopAck {
 }
 
 impl NopAck {
-    pub fn new(self,received_message_id:String) -> NopAck {
-        NopAck{
+    pub fn new() -> Self {
+        NopAck {
             command_id: 999,
-            message_id: received_message_id
+            message_id: "".to_string(),
         }
+    }
+
+    pub fn input(mut self, received_message_id: String) -> Self {
+        self.message_id = received_message_id;
+        self
     }
 }
 
 impl Display for NopAck {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f,"{}{}{}", 0x02, serde_json::to_string(&self).unwrap(), 0x03)
+        write!(f, "{}{}{}", 0x02, serde_json::to_string(&self).unwrap(), 0x03)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ use serde_json::Value;
 mod controller;
 mod tacf;
 
+use controller::NopAck;
+
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:1001")?;
 
-    let input = format!("{}{}{}", 0x02, "{'CommandId':2000,'MessageId':'030312689-000001'}", 0x03);
-    println!("Controller Request: {}", String::from_utf8_lossy(input.as_bytes()));
+    //let input = format!("{}{}{}", 0x02, "{'CommandId':2000,'MessageId':'030312689-000001'}", 0x03);
+    //println!("Controller Request: {}", String::from_utf8_lossy(input.as_bytes()));
 
-    stream.write(input.as_bytes())?;
+    //stream.write(input.as_bytes())?;
 
     let mut buffer = [0; 1024];
 
@@ -22,9 +24,13 @@ fn main() -> std::io::Result<()> {
 
 
 
+
             let response = String::from_utf8_lossy(&buffer[4..offset-1]).to_string();
             let response_json:Value = serde_json::from_str(&response).unwrap();
-            println!("TACF Response: {:#?}", response_json);
+            //println!("TACF Response: {:#?}", response_json);
+
+            let input_string = NopAck::new().input(response_json["MessageId"].to_string()).to_string();
+            stream.write(input_string.as_bytes())?;
         }
     }
 }
