e3f1d20fcc97de0db91c77156abb0582b58353c0
wangxiao <wangxiao@guadoutech.com>
2021-12-24 19:11:48 +0800

    decode success

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::io::prelude::*;
-use std::net::TcpStream;
+
 use std::thread::sleep;
 use std::time::Duration;
-use serde_json::Value;
+//use serde_json::Value;
 
 
 fn main() -> std::io::Result<()> {
+    use std::net::TcpStream;
 
-    let mut stream = TcpStream::connect("127.0.0.1:1001")?;
+    let mut stream = TcpStream::connect("127.0.0.1:1001").expect("Couldn't connect to the server...");
+
     let mut buffer = [0; 1024];
-    sleep(Duration::from_secs(1));
-
 
     let mut input = vec![2 as u8,0 as u8,50 as u8,0 as u8];
     let messages = br#"{"CommandId":2000,"MessageId":"TACF-073118689-000001","StationId":1,"StationName":"TestStation"}"#.to_vec();
@@ -18,16 +18,55 @@ fn main() -> std::io::Result<()> {
         input.push(message)
     }
     input.push(3 as u8);
-
     stream.write(&*input)?;
 
-    sleep(Duration::from_secs(1));
+    loop {
+        let offset = stream.read(&mut buffer[..])?;
 
-    let offset = stream.read(&mut buffer[..])?;
-    let response = String::from_utf8_lossy(&buffer[..offset]).to_string();
-    println!("TACF Response : {}", response);
-    println!("TACF Response : {:?}", response.as_bytes());
+        let response = String::from_utf8_lossy(&buffer[..offset]).to_string();
+
+        if response.len()>0 {
+
+            //let jsons:Vec<String>;
+
+            let items = buffer[..offset].to_vec().clone();
+
+            let mut clear_items = vec![];
+
+            for (index,item) in items.iter().enumerate() {
+                if *item == (2 as u8) && items[index+1] == (0 as u8) && items[index+2] == (50 as u8) && items[index+3] == (0 as u8){
+                    continue;
+                }
+
+                if *item == (0 as u8) && items[index+1] == (50 as u8) && items[index+2] == (0 as u8) && items[index-1] == (2 as u8){
+                    continue;
+                }
+
+                if *item == (50 as u8) && items[index+1] == (0 as u8) && items[index-2] == (2 as u8) && items[index-1] == (0 as u8){
+                    continue;
+                }
+
+                if *item == (0 as u8) && items[index-3] == (2 as u8) && items[index-2] == (0 as u8) && items[index-1] == (50 as u8){
+                    continue;
+                }
+
+                if *item == b"}"[0] && items[index+1] == (3 as u8){
+                    clear_items.push(b"}"[0]);
+                    continue;
+                }
+
+                if *item == (3 as u8) && items[index-1] == b"}"[0]{
+                    continue;
+                }
+
+                clear_items.push(items[index])
+            }
+
+            println!("{}",String::from_utf8(clear_items).unwrap());
+        }
+
+        sleep(Duration::from_millis(500));
+    }
 
     Ok(())
-
 }
