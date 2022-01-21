fa843a031abda1fbd6994c1f3371353279b8bce8
wangxiao <wangxiao@guadoutech.com>
2022-01-21 12:53:46 +0800

    v1.0.5

--- src/websocket/server.rs
+++ src/websocket/server.rs
@@ -4,7 +4,7 @@ pub struct Server{}
 
 impl Server {
     pub fn run(){
-        ws::listen(format!("127.0.0.1:8084"), |out| {
+        ws::listen(format!("0.0.0.0:8084"), |out| {
             Router {
                 sender: out,
                 inner: Box::new(TacfReceiver),
